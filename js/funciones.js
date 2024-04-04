3b9add6be3449645e89a40df42d0d580181a909b
Ernesto Montellano <emontellano@ucb.edu.bo>
2024-04-04 14:25:56 -0400

    Añadido carusel

--- js/funciones.js
+++ js/funciones.js
@@ -1,26 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
 //EMPIEZA ACÀ
-    const submenu = document.querySelector(".submenu");
-    const tieneSubmenu = submenu.parentNode;
-    const grupoSubmenus = Array.from(tieneSubmenu);
-    console.log(grupoSubmenus);
-    
-    grupoSubmenus.forEach((item) =>{
-        let icono = this.querySelector("span");
-        item.addEventListener("mouseover", () =>{
-            icono.classList.remove("caret_down");
-            icono.classList.add("caret_up");
+  let glide = new Glide(".glide",{
+    type: "carousel",
+    perView: 4,
+    gap:10,
+    autoplay:30000,
+    hoverpause:true
+  });
 
-        });
-        item.addEventListener("mouseleave", () =>{
-            icono.classList.remove("caret_up");
-            icono.classList.add("caret_down");
-            
-
-        });
-    });
-
-
+glide.mount();
    
+
 
 });
